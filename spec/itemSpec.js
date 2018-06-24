52e35ec1bd61f8681a50d2ffa27e53bfc5c88883
John Newman <newmanj903@gmail.com>
2018-06-24 19:52:53 +0100

    Adding item spec test

---
+++ spec/itemSpec.js
@@ -0,0 +1,14 @@
+describe('Item', function(){
+  var Item = require('../../src/item');
+  var item;
+
+  beforeEach(function(){
+    item = new Item("Banana Pie", 4, 3);
+  });
+
+  it('Initialises with the correct properties', function(){
+    expect(item.name).toEqual("Banana Pie");
+    expect(item.sellIn).toEqual(4);
+    expect(item.quality).toEqual(3);
+  });
+});
