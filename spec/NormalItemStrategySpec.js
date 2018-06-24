d1136c70aa19a5ad5f1b779d368a5c005e8ce41a
John Newman <newmanj903@gmail.com>
2018-06-24 20:12:48 +0100

    Strategy for normal item tests

---
+++ spec/NormalItemStrategySpec.js
@@ -0,0 +1,28 @@
+describe('NormalItemStrategy', function(){
+  var NormalItemStrategy = require('../src/SulfurusItemStrategy');
+  beforeEach(function(){
+    normalStrategy = new NormalItemStrategy();
+    normalItem = { name: "normalItem", sellIn: 1, quality: 20 }
+    normalItemAtZero = { name: "normalItem", sellIn: 1, quality: 0 }
+  })
+  it('reduces sellIn by 1', function(){
+    normalStrategy.updateItem(normalItem)
+    expect(normalItem.sellIn).toEqual(0)
+  })
+
+  it('reduces quality by 1', function(){
+    normalStrategy.updateItem(normalItem)
+    expect(normalItem.quality).toEqual(19)
+  })
+
+  it('reduces quality by 2 well sellIn is zero or below', function(){
+    normalStrategy.updateItem(normalItem)
+    normalStrategy.updateItem(normalItem)
+    expect(normalItem.quality).toEqual(17)
+  })
+
+  it('Will not let quality go beneath 0', function(){
+    normalStrategy.updateItem(normalItemAtZero)
+    expect(normalItem.quality).toEqual(0)
+  })
+})
