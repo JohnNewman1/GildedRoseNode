42560cda61042e6210d524260ae66329c054e378
John Newman <newmanj903@gmail.com>
2018-06-24 20:03:35 +0100

    Strat for sulfurus

---
+++ spec/SulfurusItemStrategySpec.js
@@ -0,0 +1,11 @@
+describe('SulfurasItemStrategy', function(){
+  var SulfurasItemStrategy = require('../SulfurasItemStrategy');
+  beforeEach(function(){
+    sulfurasStrategy = new SulfurasItemStrategy();
+    sulfuras = { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 }
+  })
+  it('Returns the item as it was', function(){
+    sulfurasStrategy.updateItem(sulfuras)
+    expect(sulfuras).toEqual({ name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 })
+  })
+})
